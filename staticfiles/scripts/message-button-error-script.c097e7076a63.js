3a6896bbf157929eeae42f05dd856c09699b28b0
karol marszalek <mkarol.4514@gmail.com>
2023-03-27 22:40:58 +0200

    sdf

--- staticfiles/scripts/message-button-error-script.c097e7076a63.js
+++
@@ -1,5 +0,0 @@
-const message = document.querySelector('.message-product')
-const messageProductBtn = document.getElementById('message-button-product')
-    messageProductBtn.addEventListener('click', ()=>{
-        message.style.display = 'none';
-});
